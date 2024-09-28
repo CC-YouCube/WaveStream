0cc18cea827db531d7e7b1dc3bb801a4e09268b9
Commandcracker <49335821+Commandcracker@users.noreply.github.com>
2024-09-28 13:50:41 +0200

    make / 200

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from os import getenv
 
-from flask import Flask
+from flask import Flask, Response
 from api.v1 import v1 as v1_blueprint
 
 app = Flask(__name__)
@@ -8,6 +8,10 @@ app = Flask(__name__)
 app.register_blueprint(v1_blueprint, url_prefix='/api/v1')
 
 # TODO: only with auth and prio "account"
+
+@app.route("/")
+def hello():
+    return Response("Hello World!")
 
 if __name__ == '__main__':
     app.run(
